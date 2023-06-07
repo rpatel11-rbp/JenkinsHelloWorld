3bb69e568aeb174cf171c7f72b3bb2ddbdace566
rpatel11-rbp <patelraj84082@gmail.com>
2023-06-07 14:45:53 +0530

    Adding new files

---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("_______***********Hello World & welcome to Jenkins***********____________");
+    }
+}
